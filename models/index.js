0e21077a1d5da9d08859ef762dd61ff85a930c2f
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-08-05 11:13:51 -0400

    comment model and routes complete

--- models/index.js
+++ models/index.js
@@ -1,12 +1,31 @@
-const User = require('./User');
+// import all models
 const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
 
+// create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id'
+});
 
-  Post.belongsTo(User, {
-    foreignKey: 'user_id',
-  });
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
