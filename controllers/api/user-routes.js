e9ba043b4526bc30bae91709c133537bcf8d3210
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-08-08 20:37:11 -0400

    updated session in post in user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -40,7 +40,13 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id
+        req.session.username = dbUserData.username
+        req.session.loggedIn = true
+      })
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
