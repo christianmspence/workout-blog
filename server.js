641c470b9dc9c71421bec50ccdbf9804a901cb5e
millerd <millerdicarlo@gmail.com>
2022-08-10 22:12:14 -0400

    main.handlebars styling added

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const routes = require('./controllers');
+// below
 const sequelize = require('./config/connection');
 const path = require('path')
 const exphb = require('express-handlebars')
+// below
 const session = require('express-session')
 const hbs = exphb.create({})
 
+// below
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
-const sess = {
+// below function
+ const sess = {
   secret: 'super secret secret',
   cookie: {},
   resave: false,
@@ -17,6 +21,7 @@ const sess = {
     db: sequelize
   })
 }
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,12 +32,17 @@ app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
+// below
 app.use(session(sess))
 
 // turn on routes
 app.use(routes);
 
+// below
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
+}); 
+
+
+app.listen(PORT, () => console.log('Now listening'));
