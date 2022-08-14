225da23eeced2274ef1b8e51fe8bba5e7d096cfd
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-08-14 16:11:32 -0400

    old code removed from comment.js

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,28 +1,3 @@
-// async function commentFormHandler(e) {
-//     e.preventDefault()
-
-//     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim()
-//     const post_id = window.location.toString().split('/')[window.location.toString().split('/').length-1]
-
-//     if (comment_text) {
-//         const res = await fetch('/api/comments', {
-//             method: 'post',
-//             body: JSON.stringify({
-//                 post_id,
-//                 comment_text
-//             }),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         })
-
-//         if (res.ok) {
-//             document.location.reload()
-//         }else {
-//             alert(res.statusText)
-//         }
-//     }
-// }
 const commentForm = document.querySelector('.comment-form')
 const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim()
 const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1]
