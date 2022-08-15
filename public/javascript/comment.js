bab6a1a4f840c613cc953e1f44b061d307cf37f9
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-08-15 12:14:54 -0400

    delete post is working. comment&post scope issues

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,46 +1,29 @@
-// async function commentFormHandler(e) {
-//     e.preventDefault()
-
-//     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim()
-//     const post_id = window.location.toString().split('/')[window.location.toString().split('/').length-1]
-
-//     if (comment_text) {
-//         const res = await fetch('/api/comments', {
-//             method: 'post',
-//             body: JSON.stringify({
-//                 post_id,
-//                 comment_text
-//             }),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         })
-
-//         if (res.ok) {
-//             document.location.reload()
-//         }else {
-//             alert(res.statusText)
-//         }
-//     }
-// }
 const commentForm = document.querySelector('.comment-form')
-const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim()
-const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1]
 
-function commentFormHandler() {
+async function commentFormHandler(e) {
+    e.preventDefault()
 
-    axios({
-        method: 'post',
-        url: '/api/comments',
-        body: {
-            comment_text,
-            post_id
-        },
-        headers: {
-            'Content-Type': 'application/json',
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim()
+    const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1]
+
+    if (comment_text) {
+        const res = await fetch('/api/comments', {
+            method: 'post',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (res.ok) {
+            document.location.reload()
+        } else {
+            alert(res.statusText)
         }
-    })
-        .then(res => document.location.reload(res))
-        .catch(err => console.error(err));
+    }
 }
+
 commentForm.addEventListener('submit', commentFormHandler)
--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,12 +1,9 @@
 const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1]
-function deleteComment() {
+function deletePost() {
     axios
         .delete(`/api/posts/${id}`)
         .then(res => document.location.replace('/profile')(res))
         .catch(err => console.error(err));
 }
 
-
-
-
-document.querySelector('.delete-post-btn').addEventListener('click', deleteComment)
+document.querySelector('.delete-post-btn').addEventListener('click', deletePost)
